5025c43282cec7fc18e2e7412c57dd7102e4d791
sbx_user1051 <sbx_user1051@169.254.92.93>
2024-07-17 00:48:01 +0000

    Wed Jul 17 00:48:01 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -33,6 +33,12 @@ def initialize_SKUs():
     SKU_dict['F'] = SKUPriceMap(10, {
                         3: 20
                     })
+    SKU_dict['G'] = SKUPriceMap(20, {})
+    SKU_dict['H'] = SKUPriceMap(10, {
+                        5: 45,
+                        10: 80
+                    })
+    SKU_dict['I'] = SKUPriceMap(35, {})
     return SKU_dict
 
 
@@ -84,6 +90,43 @@ def checkout(skus):
     | F    | 10    | 2F get one F free      |
     +------+-------+------------------------+
 
+
+
+
+    +------+-------+------------------------+
+    | Item | Price | Special offers         |
+    +------+-------+------------------------+
+    | A    | 50    | 3A for 130, 5A for 200 |
+    | B    | 30    | 2B for 45              |
+    | C    | 20    |                        |
+    | D    | 15    |                        |
+    | E    | 40    | 2E get one B free      |
+    | F    | 10    | 2F get one F free      |
+    | G    | 20    |                        |
+    | H    | 10    | 5H for 45, 10H for 80  |
+    | I    | 35    |                        |
+    | J    | 60    |                        |
+    | K    | 80    | 2K for 150             |
+    | L    | 90    |                        |
+    | M    | 15    |                        |
+    | N    | 40    | 3N get one M free      |
+    | O    | 10    |                        |
+    | P    | 50    | 5P for 200             |
+    | Q    | 30    | 3Q for 80              |
+    | R    | 50    | 3R get one Q free      |
+    | S    | 30    |                        |
+    | T    | 20    |                        |
+    | U    | 40    | 3U get one U free      |
+    | V    | 50    | 2V for 90, 3V for 130  |
+    | W    | 20    |                        |
+    | X    | 90    |                        |
+    | Y    | 10    |                        |
+    | Z    | 50    |                        |
+    +------+-------+------------------------+
+
+
+
+
     """
     SKU_dict = initialize_SKUs()
     total_cost = 0
@@ -130,6 +173,7 @@ def checkout(skus):
 
 
 print(checkout("FFFFF"))
+
 
 
 
